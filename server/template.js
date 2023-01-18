409e051c74e004f25c7414da5f73ab5e6c8ce96c
Pablo Robredo <pablorobredo@Mini-de-Pablo.home>
2023-01-18 19:33:43 +0100

    hola

--- server/template.js
+++ server/template.js
@@ -1,19 +1,19 @@
-const getDB = require('../getDB');
+// const getDB = require('../getDB');
 
-async function testing() {
-    let connection;
-    try {
-        connection = await getDB();
-        const [data] = await connection.query(`
-        SELECT minimo FROM vivienda WHERE id = 17  `);
+// async function testing() {
+//     let connection;
+//     try {
+//         connection = await getDB();
+//         const [data] = await connection.query(`
+//         SELECT minimo FROM vivienda WHERE id = 17  `);
 
-        console.log(data[0].minimo);
-    } catch (error) {
-        console.error(error.message);
-    } finally {
-        if (connection) connection.release();
-        process.exit(0);
-    }
-}
+//         console.log(data[0].minimo);
+//     } catch (error) {
+//         console.error(error.message);
+//     } finally {
+//         if (connection) connection.release();
+//         process.exit(0);
+//     }
+// }
 
-testing();
+// testing();
